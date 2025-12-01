44ad5e0fcf2e5d782a61d88762c3a840001b84b2
daniel_shaday <daniel.shaday13@gmail.com>
2025-12-01 15:46:49 +0200

    k8s files created

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #it will be filled with the flask app code
 from flask import Flask
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello Worls from Kubernetes!"
+    return "Hello World from Kubernetes!"
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
