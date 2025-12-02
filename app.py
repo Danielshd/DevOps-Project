db35f5fe1f1f5fe49050eafe86c17f8598bf1d7c
daniel_shaday <daniel.shaday13@gmail.com>
2025-12-02 17:10:27 +0200

    cicd file fixed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World from Kubernetes!"
+    return "Hello World from Kubernetes!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
