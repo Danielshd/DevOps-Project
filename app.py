a21510de7366efc9701a984756fb0d6e323b4a90
daniel_shaday <daniel.shaday13@gmail.com>
2025-12-09 13:24:46 +0200

    bonus

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-#it will be filled with the flask app code
-from flask import Flask
+from flask import Flask, jsonify
+import mysql.connector
+import os
+
 app = Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
 
 @app.route("/")
 def home():
-    return "Hello World from Kubernetes!!"
+    return "Flask + MySQL running on EKS!"
+
+@app.route("/users")
+def users():
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM users;")
+    result = cursor.fetchall()
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
