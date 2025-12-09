4c36fb9740b783d66194b50e1d41a8086f97fb66
Danielshd <166309743+Danielshd@users.noreply.github.com>
2025-12-09 11:20:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 #it will be filled with the flask app code
 from flask import Flask
+import os
+import mysql.connector
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +11,16 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "test")
+DB_USER = "root"
+DB_PASS = os.getenv("DB_ROOT_PASSWORD", "root")
+
+conn = mysql.connector.connect(
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASS,
+    database=DB_NAME
+)
